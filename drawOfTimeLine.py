a4b37660d256895b134e63eae020cf44b4e82b58
边思梦 <biansimeng@biansimengdeMacBook-Air.local>
2017-11-30 20:10:54 +0800

    Update drawing files.

--- drawOfTimeLine.py
+++ drawOfTimeLine.py
@@ -7,27 +7,37 @@ Vs = systemInformation['Vs']
 gamma = systemInformation['gamma']
 lenOfVs = len(Vs)
 
-i = 2
+i = 39
 print("V: ", Vs[i])
-index = 0  # 0: queue backlogs; 1: energy costs; 2: partition costs; 3: energy costs + gamma * partition costs.
+metric = 0  # 0: queue backlogs; 1: energy costs; 2: partition costs; 3: energy costs + gamma * partition costs.
+scheme = 3  # 0: FF; 1: FFD; 2: NAH; 3: ONRA
 
-queues = np.load("results/timeAverageOfQueueBacklogs.npy")
-energies = np.load("results/timeAverageOfEnergyCosts.npy")
-partitions = np.load("results/timeAverageOfPartitionCosts.npy")
+schemes = ["FF", "FFD", "NAH", "ONRA", ]
+
+queues = np.load("results%s/timeAverageOfQueueBacklogs.npy" % schemes[scheme])
+energies = np.load("results%s/timeAverageOfEnergyCosts.npy" % schemes[scheme])
+partitions = np.load("results%s/timeAverageOfPartitionCosts.npy" % schemes[scheme])
 
 x = list(range(maxTime))
 
-ys = [queues[i], energies[i], partitions[i], np.array(energies[i]) + gamma * np.array(partitions[i])]
+
+if scheme == 3:
+    ys = [queues[i], energies[i], partitions[i], np.array(energies[i]) + gamma * np.array(partitions[i])]
+    plt.xlabel("Time-slot (V=" + str(Vs[i]) + ")")
+else:
+    ys = [queues, energies, partitions, np.array(energies) + gamma * np.array(partitions)]
+    plt.xlabel("Time-slot")
+
 labels = ["Time-Average Queue Backlogs",
           "Time-Average Energy Cost",
           "Time-Average Partition Cost",
           "Time-Average Energy and Partition Cost"]
 colors = ['r', 'g', 'b', 'y']
 
-plt.xlabel("Time-slot (V=" + str(Vs[i]) + ")")
-plt.ylabel(labels[index])
+plt.ylabel(labels[metric])
+plt.title(schemes[scheme])
 
-plt.plot(x, ys[index], colors[index])
+plt.plot(x, ys[metric], colors[metric])
 plt.show()
 
 print("test")
--- drawOfVs.py
+++ drawOfVs.py
@@ -7,11 +7,11 @@ gamma = systemInformation['gamma']
 Vs = systemInformation['Vs']
 lenOfVs = len(Vs)
 
-index = 1  # 0: queue backlogs; 1: energy costs; 2: partition costs; 3: energy costs + gamma * partition costs.
+metric = 3  # 0: queue backlogs; 1: energy costs; 2: comm. costs; 3: energy costs + gamma * partition costs.
 
-queues = np.load("results_old/timeAverageOfQueueBacklogs.npy")
-energies = np.load("results_old/timeAverageOfEnergyCosts.npy")
-partitions = np.load("results_old/timeAverageOfPartitionCosts.npy")
+queues = np.load("resultsONRA/timeAverageOfQueueBacklogs.npy")
+energies = np.load("resultsONRA/timeAverageOfEnergyCosts.npy")
+partitions = np.load("resultsONRA/timeAverageOfPartitionCosts.npy")
 
 x = Vs
 ys = [[], [], [], []]
@@ -21,18 +21,22 @@ for i in range(lenOfVs):
     ys[2].append(partitions[i, maxTime - 1])
     ys[3].append(energies[i, maxTime-1] + gamma * partitions[i, maxTime - 1])
 
-print ys[1]
+# print(ys[1])
 
-labels = ["Time-Average Queue Backlogs at Time-slot " + str(maxTime),
-          "Time-Average Energy Cost at Time-slot " + str(maxTime),
-          "Time-Average Partition Cost at Time-slot " + str(maxTime),
-          "Time-Average Energy and Partition Cost at Time-slot " + str(maxTime)]
-colors = ['r', 'g', 'b', 'y']
+ylabels = ["Time-Avg Queue Backlog Size $(\\times 10^4)$",
+          "Time-Avg Energy Cost",
+          "Time-Avg Communication Cost",
+          "Time-Avg Total Cost"]
+colors = ['ro-', 'go-', 'bo-', 'mo-']
 
-plt.xlabel("V")
-plt.ylabel(labels[index])
+plt.figure(figsize=(10, 8))
+plt.xlabel("V", fontsize=30)
+plt.ylabel(ylabels[metric], fontsize=30)
+plt.plot(x, ys[metric], colors[metric], linewidth=2)
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+if metric == 0:
+    plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
 
-plt.plot(x, ys[index], colors[index])
+
 plt.show()
-
-print("test")
